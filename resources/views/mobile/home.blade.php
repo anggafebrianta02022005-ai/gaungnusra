fa450b55ce5bb98b7216ebd87f9c21263c913b79
Angga Febrianta <anggafebrianta02022005@gmal.com>
2026-02-09 11:23:18 +0700

    perbaiki mobile

--- resources/views/mobile/home.blade.php
+++ resources/views/mobile/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#F1F5F9">
-    <title>{{ $company->name ?? 'Portal Berita' }}</title>
+    <title>Gaung Nusra - Media Online</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
