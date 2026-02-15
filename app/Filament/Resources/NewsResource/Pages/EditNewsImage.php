b616f3d47bd17261a8bfabc69c52112178e4c2b1
Angga Febrianta <anggafebrianta02022005@gmal.com>
2026-02-16 03:00:04 +0700

    Gambar Mode Hp

--- app/Filament/Resources/NewsResource/Pages/EditNewsImage.php
+++ app/Filament/Resources/NewsResource/Pages/EditNewsImage.php
@@ -9,9 +9,13 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Actions\Action;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Concerns\InteractsWithForms;
 
-class EditNewsImage extends Page
+class EditNewsImage extends Page implements HasForms
 {
+    use InteractsWithForms;
+
     protected static string $resource = NewsResource::class;
     protected static string $view = 'filament.resources.news-resource.pages.edit-news-image';
 
@@ -21,7 +25,12 @@ class EditNewsImage extends Page
     public function mount(News $record): void
     {
         $this->record = $record;
-        $this->form->fill($record->toArray());
+        
+        // Pastikan data awal terisi agar jika tidak diedit salah satu, gambar lama tidak hilang
+        $this->form->fill([
+            'image' => $record->image,
+            'thumbnail' => $record->thumbnail,
+        ]);
     }
 
     public function form(Form $form): Form
@@ -31,6 +40,7 @@ class EditNewsImage extends Page
                 FileUpload::make('image')
                     ->label('Potong Gambar Header')
                     ->image()
+                    ->directory('news-main') // Pastikan direktori sama dengan di Resource utama
                     ->imageEditor()
                     ->imageEditorMode(1)
                     ->imageEditorAspectRatios(['16:9', '4:3', '1:1'])
@@ -40,6 +50,7 @@ class EditNewsImage extends Page
                 FileUpload::make('thumbnail')
                     ->label('Potong Thumbnail')
                     ->image()
+                    ->directory('news-thumbnails') // Pastikan direktori sama dengan di Resource utama
                     ->imageEditor()
                     ->imageEditorMode(1)
                     ->imageEditorAspectRatios(['16:9'])
@@ -51,8 +62,19 @@ class EditNewsImage extends Page
 
     public function save()
     {
-        $this->record->update($this->data);
-        Notification::make()->title('Gambar Berhasil Disimpan')->success()->send();
+        // 1. Ambil data yang sudah divalidasi dari form
+        $formData = $this->form->getState();
+
+        // 2. Update record database
+        $this->record->update($formData);
+
+        // 3. Notifikasi sukses
+        Notification::make()
+            ->title('Gambar Berhasil Disimpan')
+            ->success()
+            ->send();
+
+        // 4. Kembali ke halaman daftar
         return redirect($this->getResource()::getUrl('index'));
     }
 }
